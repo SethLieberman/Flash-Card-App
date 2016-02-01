6a013891ef986582979daa4a038a9642bd37b045
Richard Mable <richardbmable@gmail.com>
2016-02-01 15:46:17 -0500

    added more cards actions and the cards view holds all the views actions

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -5,7 +5,7 @@ class CardsController < ApplicationController
 
   #for showing all the cards
   def index
-    @card = Card.all
+    @cards = Card.all
   end
 
   #action for a new card for the view (card/new.html.erb)
@@ -17,10 +17,10 @@ class CardsController < ApplicationController
   def create
     @card = Card.new
     #create the card
-    @card.create(card_params)
+    @card = Card.create(card_params)
     #redirections if card was saved or not along with flash messaging
     if @card.save
-      flash[:notice] = "Car was created"
+      flash[:notice] = "Card was created"
       #redirect to the cards index page
       redirect_to cards_url
     else
@@ -31,15 +31,46 @@ class CardsController < ApplicationController
 
   #for showing the currently selected card
   def show
-    # @card = 
+    @card = Card.find(card_params[:id])
   end
 
-  #editing an already created card
+  #action for view for editing an already created card
   def edit
-    # @card = 
+    #set @card to the current card, which was linked from the cards index view
+    @card = Card.find(params[:id])
+    
   end
 
-  
+  #action for updating a card
+  def update
+    @card = Card.find(params[:id])
+   #update the card with the submitted params
+    if @card.update(card_params)
+    #if succesful display message
+      flash[:notice] = "Card was updated"
+      #redirect to the that card's page
+      redirect_to card_url
+    else
+      #if something went wrong, redirect to that card's page
+      flash[:alert] = "Something went wrong."
+      redirect_to card_url
+    end
+  end
+
+  #action for destroying a card
+  def destroy
+    #assign @card to the current card
+    @card = Card.find(params[:id])
+    #if succesful display message
+    if @card.destroy
+      flash[:notice] = "Card was destoryed"
+      #redirect to cards index
+      redirect_to cards_url
+    else
+      flash[:alert] = "Something went wrong."
+      redirect_to cards_url
+    end
+  end
 
   
   #strong params
@@ -47,6 +78,6 @@ class CardsController < ApplicationController
 
   #require the params of card and permit both question and answer to be written in DB
   def card_params 
-    params.require(:card).permit(:question, :answer)
+    params.require(:card).permit(:question, :answer, :level_id)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,14 @@
 
 Card.create([
 	{question:"What does HTML stand for", answer:"Hypertext Markup Language", level_id: 1, admin_id: 1},
-	{question:"What is <>", answer:"Carrot" level_id: 1, admin_id: 1}
-	{question:"What is love", answer:"baby don't hurt me" level_id: 1, admin_id: 1}
-	{question:"Hello", answer:"It's me" level_id: 1, admin_id: 1}
-	{question:"Life", answer:"42" level_id: 2, admin_id: 1}
-	{question:"Blah baljh", answer:"afawefaew" level_id: 2, admin_id: 1}
-	{question:"awhefl;a", answer:"wgna'wenglanw" level_id: 3, admin_id: 1}
-	{question:"kjabef;kjba;ewf", answer:"a;oiwefhoa" level_id: 4, admin_id: 1}
-	{question:"a;kaewf;ajk", answer:"a;oweifha'owe" level_id: 5, admin_id: 1}
+	{question:"What is <>", answer:"Carrot", level_id: 1, admin_id: 1},
+	{question:"What is love", answer:"baby don't hurt me", level_id: 1, admin_id: 1},
+	{question:"Hello", answer:"It's me", level_id: 1, admin_id: 1},
+	{question:"Life", answer:"42", level_id: 2, admin_id: 1},
+	{question:"Blah baljh", answer:"afawefaew", level_id: 2, admin_id: 1},
+	{question:"awhefl;a", answer:"wgna'wenglanw", level_id: 3, admin_id: 1},
+	{question:"kjabef;kjba;ewf", answer:"a;oiwefhoa", level_id: 4, admin_id: 1},
+	{question:"a;kaewf;ajk", answer:"a;oweifha'owe", level_id: 5, admin_id: 1}
 	])
 
 User.create([
@@ -24,6 +24,6 @@ User.create([
 	])
 
 Admin.create([
-	{email:"skwildcat7@gmail.com", password: "12345678"}
+	# {email:"skwildcat7@gmail.com", password: "12345678"}
 	])
 
