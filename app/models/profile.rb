1acde5135fe6f880258b3f85765ad542b101d819
Seth Lieberman <shefseth@gmail.com>
2016-02-15 11:40:49 -0500

    updated avatar

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,6 +2,6 @@ class Profile < ActiveRecord::Base
 	belongs_to :user
 
 	 #for paperclip
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing1.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
